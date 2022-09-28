0de611c0886b68b868cd58a7a9d1d7d45dc9edd5
MacaronnyPizza <114050926+MacaronnyPizza@users.noreply.github.com>
2022-09-28 08:20:49 +0300

    Repeats in game

--- game.py
+++ game.py
@@ -1,13 +1,23 @@
-human_turn = input('Input human turn:')
-computer_turn = input('Input computer turn:')
+import random
+turns = ['rock','paper','scissors']
 
-if human_turn == computer_turn:
-    print('Draw!')
-elif human_turn == 'rock' and computer_turn == 'scissors':
-    print('Humanity wins!')
-elif human_turn == 'paper' and computer_turn == 'rock':
-    print('People wins!')
-elif human_turn == 'scissors' and computer_turn == 'paper':
-    print('You win!')
-else:
-    print('Machine are winning!')
+while(True):
+    human_turn = input('Enter your turn, or type exit:')
+    computer_turn = random.choice(turns)
+
+    if human_turn == 'exit':
+        print('Thanks for playing!')
+        break
+
+    print(f'Human:{human_turn} vs. Computer:{computer_turn}')
+
+    if human_turn == computer_turn:
+        print('Draw!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+        print('Humanity wins!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+        print('Humanity wins!')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+        print('You win!')
+    else:
+        print('Machine are winning!')
